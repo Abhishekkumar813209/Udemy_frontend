9582fc46d8a5649e1ad1396fb19c345e454611a1
Bibash Roy <bibhash@gmail.com>
2023-10-30 21:24:14 +0530

    added last part

---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+import {configureStore} from "@reduxjs/toolkit"
+import { profileReducer, subscriptionReducer, userReducer } from "./reducers/userReducer";
+import { courseReducer } from "./reducers/courseReducers";
+import { adminReducer } from "./reducers/adminReducer";
+import { otherReducer } from "./reducers/otherReducer";
+
+const store = configureStore({
+reducer:{
+    user:userReducer,
+    profile:profileReducer,
+    course:courseReducer,
+    subscription:subscriptionReducer,
+    admin:adminReducer,
+    other:otherReducer
+}
+})
+
+export default store;
+
+export const server = "https://backend-tdl9.onrender.com"
